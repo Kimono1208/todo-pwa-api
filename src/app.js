38177954cff32c39c29dff22bedd2b6293a295a9
Kimono1208 <128449667+Kimono1208@users.noreply.github.com>
2026-02-25 11:05:32 -0600

    base de datos ruta app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import morgan from 'morgan';
 import taskRoutes from './routes/task.routes.js';
 import authRoutes from './routes/auth.routes.js';
-import {connectToDB} from '../src/db/connect.js';
+import {connectToDB} from './db/connect.js';
 
 const app = exxpress();
 
