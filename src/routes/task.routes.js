1fa447eb335a8e21e1b928928a06c8dcf5ddbca1
Kimono1208 <128449667+Kimono1208@users.noreply.github.com>
2026-02-26 11:55:59 -0600

    edit task.routes.js

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,11 +3,11 @@ import {auth} from "../middleware/auth.js"
 import {list, create,update,remove} from "../controller/task.controller.js"
 
 const router = Router();
-router.use(auth) 
-router.get('/', list)
-router.post('/', create)
-router.put('/id', update) 
-router.delete('/id', remove)
-// router.post('/bulksynk', bulksynk)
+router.use(auth) ;
+router.get('/', list);
+router.post('/', create);
+router.put('/:id', update);
+router.delete('/:id', remove);
+router.post('/bulksync', bulksync);
 
 export default router;
